e13fb7ffba57d290e5adbd85364e0c4d255a6d23
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-11-10 13:46:55 +0100

    without Is

--- src/main/scala/DNegW.scala
+++ src/main/scala/DNegW.scala
@@ -2,30 +2,33 @@ package circuits
 
 import cats.evidence.Is
 
-case class DNegW[P[_], T](p: P[T], isNeg: Option[T Is Boolean])
+case class DNegW[P[_], T](p: P[T], isNeg: DNegW.IsNeg[T])
 
 object DNegW{
 
+  sealed abstract class IsNeg[T]
+  case class No[T]() extends IsNeg[T]
+  case class Yes() extends IsNeg[Boolean]
+
   def run[P[_], T](c: DNegW[P, T])(implicit C: Circuit[P]): P[T] =
-    c.isNeg.fold(c.p){
-      isBool => isBool.flip.substitute[P](
-        C.not(isBool.substitute[P](c.p))
-      )
+    c.isNeg match {
+      case No() => c.p
+      case Yes() => C.not(c.p)
     }
 
   implicit def DNegWCircuit[P[_]: Circuit] = new Circuit[DNegW[P, ?]]{
 
     def lit(b: Boolean): DNegW[P, Boolean] =
-      DNegW(Circuit[P].lit(b), None)
+      DNegW(Circuit[P].lit(b), No[Boolean]())
 
     def and(p1: DNegW[P, Boolean], p2: DNegW[P, Boolean]): DNegW[P, Boolean] =
-      DNegW(Circuit[P].and(DNegW.run(p1), DNegW.run(p2)), None)
+      DNegW(Circuit[P].and(DNegW.run(p1), DNegW.run(p2)), No[Boolean]())
 
     def or(p1: DNegW[P, Boolean], p2: DNegW[P, Boolean]): DNegW[P, Boolean] =
-      DNegW(Circuit[P].or(DNegW.run(p1), DNegW.run(p2)), None)
+      DNegW(Circuit[P].or(DNegW.run(p1), DNegW.run(p2)), No[Boolean]())
 
     def not(p: DNegW[P, Boolean]): DNegW[P, Boolean] =
-      if (p.isNeg.isDefined) DNegW(p.p, None)
-      else DNegW(DNegW.run(p), Option(Is.refl[Boolean]))
+      if (p.isNeg == Yes()) DNegW(p.p, No[Boolean]())
+      else DNegW(DNegW.run(p), Yes())
   }
 }
